e6abb418f21f7d5b4db1f01fe4c5b126b3092a45
Vitalik <vitalik1802@gmail.com>
2021-06-21 14:19:33 +0300

    Initial

---
+++ Lesson 14/script.js
@@ -0,0 +1,57 @@
+/* function even_or_odd(number) {
+    return number % 2 == 0 ? "Even" : "Odd"
+  } */
+
+  /* function makeNegative(num) {
+    if (num > 0) {
+      return num * -1;
+    } else {
+      return num;
+    }
+  } */
+
+ /* function greet(name){
+    if(name === "Johnny") {
+      return "Hello, my love!";
+    } else {
+      return "Hello, " + name + "!";
+  }
+} */
+  
+
+/* function lovefunc(flower1, flower2) {
+    let item1 = (flower1 % 2 == 0) && (flower2 % 2 != 0);
+    let item2 = (flower2%2==0) && (flower1%2!=0);
+  
+    if(item1 || item2) return true;
+              return false;
+    } */
+  
+    /* function boolToWord(bool){
+        let item = bool.toString();
+        if (item == 'true') {
+              return "Yes";
+         } else {
+              return "No";
+         }
+      } */
+      
+
+      /* function rentalCarCost(d) {
+        const base = 40
+        if (d >= 7) {
+          return d * base - 50;
+        } 
+        if (d >= 3) {
+          return d * base - 20;
+        }  
+        return d * base;
+      } */
+      
+      /* function basicOp(operation, value1, value2) {
+            if(operation == '+')return value1 + value2;
+            if(operation == '-')return value1 - value2;
+            if(operation == '*')return value1 * value2;
+            if(operation == '/')return value1 / value2;
+        } */ 
+      
